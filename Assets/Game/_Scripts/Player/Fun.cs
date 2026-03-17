1e21de410ec81bba2ac310463a254833ce68d8f5
ToineF <113095213+ToineF@users.noreply.github.com>
2026-03-17 16:14:23 +0100

    🎯

---
+++ Assets/Game/_Scripts/Player/Fun.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Gun, mais j'ai écrit Fun sans faire exprès c'est plus drôle
+/// </summary>
+public class Fun : MonoBehaviour
+{
+    private void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("BOOM");
+        }
+    }
+}
