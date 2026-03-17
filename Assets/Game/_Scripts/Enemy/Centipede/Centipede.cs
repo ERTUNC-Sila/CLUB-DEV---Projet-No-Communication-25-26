0f39b993cb266f74724fdd255871cd9e795884b5
ToineF <113095213+ToineF@users.noreply.github.com>
2026-03-17 20:53:38 +0100

    🎩

---
+++ Assets/Game/_Scripts/Enemy/Centipede/Centipede.cs
@@ -0,0 +1,46 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.Splines;
+
+
+public class Centipede : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Transform _targetTransform;
+
+    [SerializeField] private GameObject _eyePrefab;
+    [SerializeField] private SplineContainer _spline;
+
+    [Header("Parameters")]
+    [SerializeField] private int _eyesNumber = 7;
+
+    [SerializeField] private float _targetOffset = 0.01f;
+    [SerializeField] private float _splineSpeed = 15;
+
+#if UNITY_EDITOR
+    public void CreateCentipede()
+    {
+        // Destroy previous children
+        int childCount = transform.childCount;
+        for (int i = childCount - 1; i >= 0; i--)
+        {
+            GameObject.DestroyImmediate(transform.GetChild(i).gameObject);
+        }
+
+        // Spawn new children
+        for (int i = 0; i < _eyesNumber; i++)
+        {
+            var instance = GameManager.Instantiate(_eyePrefab, _targetTransform);
+            var splineAnimate = instance.AddComponent<SplineAnimate>();
+            splineAnimate.Container = _spline;
+            splineAnimate.AnimationMethod = SplineAnimate.Method.Speed;
+            splineAnimate.MaxSpeed = _splineSpeed;
+            splineAnimate.StartOffset = _targetOffset * i;
+            //Selection.activeGameObject = instance;
+        }
+
+        // Re-select target transform
+        //Selection.activeGameObject = _targetTransform.gameObject;
+    }
+#endif
+}
---
+++ Assets/Game/_Scripts/Enemy/Centipede/Editor/CentipedeEditor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Centipede))]
+public class CentipedeEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        Centipede centipede  = target as Centipede;
+        
+        base.OnInspectorGUI();
+        
+        EditorGUILayout.Space();
+
+        if (GUILayout.Button("Create Centipede"))
+        {
+            centipede.CreateCentipede();
+        }
+    }
+}
